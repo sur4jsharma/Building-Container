4758ef8954647652aef198b9209efbf21dd720de
sur4jsharma <iitb.suraj@gmail.com>
2021-04-16 06:10:23 +0530

    mount done

--- containerDemo.c
+++ containerDemo.c
@@ -1,4 +1,5 @@
 #define _GNU_SOURCE
+#include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/utsname.h>
 #include <sched.h>
@@ -8,6 +9,8 @@
 #include <unistd.h>
 #include <sys/mount.h>
 #include <error.h>
+#include <sys/syscall.h>
+#include <fcntl.h>
 
 #define STACK_SIZE (1024 * 1024)    /* Stack size for cloned child */
 
@@ -18,23 +21,28 @@
 
 void mount_namespace(const char *file_system)
 {
-    mount(file_system,file_system,"ext4",MS_BIND,"");
+    if(mount(file_system,file_system,"ext4",MS_BIND,"") == -1)
+        errExit("mount in mount_namespace");
     chdir(file_system);
     const char *old_fs = ".old_fs";
-    mkdir(old_fs,0777);
+
+    if( rmdir(old_fs) == -1)
+        errExit("rmdir old_fs");
+
+    if( mkdir(old_fs,0777) == -1)
+        errExit("mkdir old_fs");
+        
     pivot_root(".",old_fs);
+        // errExit("pivot root")
     chdir("/");
 }
 
 void pid_namespace()
 {
     const char *mount_point = "/proc";
-    if (mount_point != NULL) 
-    {
-        if (mount("proc", mount_point, "proc", 0, NULL) == -1)
-            errExit("mount");
-        printf("Mounting procfs at %s\n", mount_point);
-    }
+    if (mount("proc", mount_point, "proc", 0, NULL) == -1)
+        errExit("mount in pid namespace");
+    // printf("Mounting procfs at %s\n", mount_point);
 }
 
 void uts_namespace(const char *child_host_name)
@@ -50,19 +58,36 @@ void uts_namespace(const char *child_host_name)
     printf("nodename in child uts namespace: %s\n", uts.nodename);
 }
 
+void network_namespace(const char *netspace_name)
+{
+    int fd = open(netspace_name,O_RDONLY | O_CLOEXEC);
+    if(fd == -1)
+        errExit("open");
+    int ret_no = setns(fd,0);
+    // printf("%d\n",temp);
+    if(ret_no == -1)
+        errExit("setns");
+}
+
 int namespace_handler(void *args)
 {
     printf("NAMESPACE HANDLER\n");
     char **argv = (char **)args;
 
     const char *file_system = argv[1];
+    // const char *file_system = "rootfs";
     mount_namespace(file_system);
 
     pid_namespace();
-    // const char *child_host_name = argv[1];
-    // uts_namespace(child_host_name);
 
-    execlp("bin/bash","bin/bash",NULL);
+    const char *child_host_name = argv[2];
+    uts_namespace(child_host_name);
+    
+    const char* nspace_name = argv[3];
+    network_namespace(nspace_name);
+    /* run bash shell in separete namespace */ 
+    execlp("/bin/bash","/bin/bash",NULL);
+    
     return 0;
 }
 
@@ -71,8 +96,9 @@ int main(int argc, char *argv[])
     static char child_stack[STACK_SIZE];
     pid_t child_pid;
     struct utsname uts;
-
-    if (argc < 2) {
+    printf("ENTER FILE_SYSTEM, HOST_NAME, NETWORK_NAMESPACE IN ORDER\n");
+    if (argc < 3) 
+    {
         fprintf(stderr, "Usage: %s <child-hostname>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -89,7 +115,7 @@ int main(int argc, char *argv[])
     {
         errExit("clone");
     }
-    printf("PID of child created by clone() is %ld\n", (long) child_pid);
+    // printf("PID of child created by clone() is %ld\n", (long) child_pid);
 
     /* Display the hostname in parent's UTS namespace. This will be 
        different from the hostname in child's UTS namespace. */
