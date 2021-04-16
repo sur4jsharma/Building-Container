753f4f8102f62fe3b7d32ee6126adcac8cc73a86
sur4jsharma <iitb.suraj@gmail.com>
2021-04-17 01:13:50 +0530

    test case added

--- run_container1.sh
+++ run_container1.sh
@@ -1,1 +1,1 @@
-sudo ./container rootfs container1 netns1 veth0 veth1 192.168.1.10 192.168.1.11
+sudo ./container rootfs container1 netns1 veth0 veth1 192.168.1.10 192.168.1.11 proc
--- run_container2.sh
+++ run_container2.sh
@@ -1,1 +1,1 @@
-sudo ./container rootfs container2 netns2 veth2 veth3 192.168.1.12 192.168.1.13
+sudo ./container rootfs container2 netns2 veth2 veth3 192.168.1.12 192.168.1.13 proc
